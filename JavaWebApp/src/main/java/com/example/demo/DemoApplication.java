1f7746023905be4600b89e69176342cfa4e95235
Handful Jallah <pjhandful@hotmail.com>
2022-10-26 09:47:33 -0500

    fix for syntax issue

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		String name = "pammie";
+		String name = "vamsi";
 
 		Systemn.out.println(name); 
 	}
